8bec54dc11a0a79a5330b2b7fbaec1394a46a91e
ajalan065 <arpitjalan.drupal@gmail.com>
2016-09-16 19:00:11 +0530

    Basic Layout of the Graph chart added

---
+++ AndroidStudioProjects/ITS/app/src/main/java/com/cse/its/its/LineGraph.java
@@ -0,0 +1,84 @@
+package com.cse.its.its;
+
+import android.content.Context;
+import android.graphics.Color;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.chart.PointStyle;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
+
+/**
+ * Created by ajalan on 16/9/16.
+ */
+public class LineGraph {
+    private XYSeries xSeries, ySeries;
+    XYMultipleSeriesDataset dataset;
+    XYSeriesRenderer xRenderer, yRenderer;
+    XYMultipleSeriesRenderer multiRenderer;
+    private GraphicalView mChart;
+
+    /**
+     * Constructor called.
+     */
+    LineGraph() {
+        dataset = new XYMultipleSeriesDataset();
+
+        xSeries = new XYSeries("X");
+        ySeries = new XYSeries("Y");
+
+        // Add new XY series to the list
+        dataset.addSeries(xSeries);
+        dataset.addSeries(ySeries);
+
+        // Renderer for the X series
+        xRenderer = new XYSeriesRenderer();
+        xRenderer.setColor(Color.RED);
+        xRenderer.setPointStyle(PointStyle.CIRCLE);
+        xRenderer.setFillPoints(true);
+        xRenderer.setLineWidth(1);
+        xRenderer.setDisplayChartValues(false);
+        xRenderer.setLineWidth(3f);
+
+        // Renderer for the Y series
+        yRenderer = new XYSeriesRenderer();
+        yRenderer.setColor(Color.GREEN);
+        yRenderer.setPointStyle(PointStyle.CIRCLE);
+        yRenderer.setFillPoints(true);
+        yRenderer.setLineWidth(1);
+        yRenderer.setDisplayChartValues(false);
+        yRenderer.setLineWidth(3f);
+
+        // Renders the display and the orientation of the chart
+        multiRenderer = new XYMultipleSeriesRenderer();
+        multiRenderer.setXLabels(0);
+        multiRenderer.setLabelsColor(Color.RED);
+        multiRenderer.setChartTitle("t vs (x,y)");
+        multiRenderer.setXTitle("Sensor Data");
+        multiRenderer.setYTitle("Intensity Value");
+        multiRenderer.setZoomButtonsVisible(true);
+        //multiRenderer.setScale(0.25f);
+        for(int i=-100;i<100;i++){
+            multiRenderer.addYTextLabel(i,(i*2)+"");
+        }
+        multiRenderer.addSeriesRenderer(xRenderer);
+        multiRenderer.addSeriesRenderer(yRenderer);
+        multiRenderer.setInScroll(true);
+        multiRenderer.setZoomEnabled(true);
+    }
+
+    /**
+     * Get the graph based on the chart rendered
+     * @param context
+     * @return GraphicalView
+     */
+    public GraphicalView getGraph(Context context){
+
+        mChart = ChartFactory.getLineChartView(context, dataset, multiRenderer);
+        return mChart;
+    }
+
+}
--- AndroidStudioProjects/ITS/app/src/main/java/com/cse/its/its/MainActivity.java
+++ AndroidStudioProjects/ITS/app/src/main/java/com/cse/its/its/MainActivity.java
@@ -8,16 +8,31 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.achartengine.GraphicalView;
+
+/**
+ * Created by ajalan on 10/9/16.
+ */
 public class MainActivity extends Activity implements SensorEventListener {
     //SensorManager lets you access the device's sensors
     //declare Variables
+    private boolean started = false;
     private SensorManager sensorManager;
-    TextView tVProximity;
+    TextView tvLight, lightValue;
+    Sensor lightSensor;
+    Button start, stop;
+    private GraphicalView view;
+    private LineGraph line;
+    private LinearLayout layout;
+    
 
     /** Called when the activity is first created. */
     @Override
@@ -25,27 +40,59 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
-        tVProximity = (TextView)findViewById(R.id.tVProximity);
+        tvLight = (TextView)findViewById(R.id.tvLight);
+        lightValue = (TextView)findViewById(R.id.lightValue);
         //create instance of sensor manager and get system service to interact with Sensor
         sensorManager= (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        Sensor proximitySensor= sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        lightSensor= sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        start = (Button)findViewById(R.id.start);
+        stop = (Button)findViewById(R.id.stop);
+        layout = (LinearLayout) findViewById(R.id.chart_container);
 
+        start.setEnabled(true);
+        stop.setEnabled(false);
 
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                started = true;
+                start.setEnabled(false);
+                stop.setEnabled(true);
+                Toast.makeText(getApplicationContext(), "Light Sensor enabled", Toast.LENGTH_SHORT).show();
+                sensorManager.registerListener(MainActivity.this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                layout.removeAllViews();
+                line = new LineGraph();
+                view = line.getGraph(MainActivity.this);
+                layout.addView(view);
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                started = false;
+                start.setEnabled(true);
+                stop.setEnabled(false);
+                Toast.makeText(getApplicationContext(), "Light Sensor disabled", Toast.LENGTH_SHORT).show();
+                sensorManager.unregisterListener(MainActivity.this);
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        // register this class as a listener for the Proximity Sensor
-        sensorManager.registerListener(this,
-                sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT),
-                SensorManager.SENSOR_DELAY_NORMAL);
+        if (started) {
+            sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
+
     @Override
     protected void onPause() {
-        // unregister listener
         super.onPause();
-        sensorManager.unregisterListener(this);
+        if (started) {
+            sensorManager.unregisterListener(this);
+        }
     }
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -54,9 +101,10 @@ public class MainActivity extends Activity implements SensorEventListener {
     // called when sensor value have changed
     @Override
     public void onSensorChanged(SensorEvent event) {
+        lightValue.setText("Light Intensity is " + event.values[0] + " luxes");
         if (event.values[0] == 0) {
 
-            Toast.makeText(getApplicationContext(), "sensor in 0",Toast.LENGTH_LONG).show();
+            //Toast.makeText(getApplicationContext(), "Light Value " + event.values[0],Toast.LENGTH_LONG).show();
             WindowManager.LayoutParams params = getWindow().getAttributes();
             params.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
             params.screenBrightness = 0;
@@ -65,7 +113,7 @@ public class MainActivity extends Activity implements SensorEventListener {
 
         } else {
 
-            Toast.makeText(getApplicationContext(), "sensor in 1",Toast.LENGTH_LONG).show();
+            //Toast.makeText(getApplicationContext(), "Light Value" + event.values[0],Toast.LENGTH_LONG).show();
             WindowManager.LayoutParams params = getWindow().getAttributes();
 
             params.screenBrightness = -1;
