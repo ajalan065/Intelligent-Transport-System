23e4773c4845ed2e17d82d3e95d3e5a03b7ebca2
ajalan065 <arpitjalan.drupal@gmail.com>
2016-09-23 19:27:50 +0530

    Added the Launch Activity

---
+++ AndroidStudioProjects/ITS/app/src/main/java/com/cse/its/its/LaunchActivity.java
@@ -0,0 +1,27 @@
+package com.cse.its.its;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    private Handler mHandler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        // Auto start the main activity after 3 seconds.
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+                // Finish the current activity
+                finish();
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+}
